cec9b7f9310eada305c52981cdbd0d359d5fb23e
brian cooper <44455883+prophoss@users.noreply.github.com>
2018-11-30 14:30:27 -0600

    Add files via upload

---
+++ tut.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := [5]int{25, 50, 75, 100, 125}
+	fmt.Println("emp:", a)
+	fmt.Println("set:", a)
+	fmt.Println("get:", a[4], a[2])
+
+	var twoD [2][3]int
+	for b := 0; b < 2; b++ {
+		for c := 0; c < 3; c++ {
+			twoD[b][c] = b + c
+
+		}
+	}
+	fmt.Println("2d:", twoD)
+
+}
